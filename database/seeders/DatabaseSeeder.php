9898d1185f604c546d5282a71d47aac22aea0e9a
Shrestha Shome Ullas <shrestha.shome@gmail.com>
2025-07-27 01:11:45 +0600

    feat: Setup Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $categories = ['Technology', 'Health', 'Lifestyle', 'Education', 'Travel', 'Food', 'Finance', 'Entertainment', 'Sports', 'Science', 'Politics', 'Environment', 'Art', 'Culture', 'Business', 'Gaming', 'Fashion', 'History', 'Music', 'Photography'];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+
+        Post::factory(100)->create();
     }
 }
